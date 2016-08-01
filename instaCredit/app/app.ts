60252c005fad6962bd8e789b9fac2273d1249226
SonLe <letuthanhson@gmail.com>
2016-08-01 18:11:05 +0000

    changes for insta

--- instaCredit/app/app.ts
+++ instaCredit/app/app.ts
@@ -1,7 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {HomePage} from './pages/home/home';
 import {ListPage} from './pages/list/list';
 
 
@@ -12,7 +12,7 @@ class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage: any = HelloIonicPage;
+  rootPage: any = HomePage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
@@ -23,7 +23,7 @@ class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'InstaCredit', component: HomePage },
       { title: 'My First List', component: ListPage }
     ];
   }
